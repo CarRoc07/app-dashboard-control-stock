385b84f2a097586401fa3a827fdffd088dff1582
CarRoc07 <carlovhd@gmail.com>
2023-09-15 00:03:02 -0300

    add icons

--- src/components/ProductInfo.jsx
+++ src/components/ProductInfo.jsx
@@ -1,6 +1,7 @@
 'use client'
 import { useRouter } from 'next/navigation'
 import React from 'react'
+import { RiDeleteBinLine, RiEdit2Line } from 'react-icons/ri'
 
 const ProductInfo = ({ _id, product, stock, costo, openModal }) => {
     const router = useRouter()
@@ -10,11 +11,11 @@ const ProductInfo = ({ _id, product, stock, costo, openModal }) => {
             <p className='text-lg font-semibold w-[150px] overflow-x-hidden'> { product } </p>
             <p className='text-lg font-semibold w-[20px] text-center'> { stock } </p>
             <p className='text-lg font-semibold text-center w-[100px]'> ${costo} </p>
-            <p className='text-lg font-semibold text-center w-[100px]'> ${costo * 1.30}  </p>
-            <p className='text-lg font-semibold text-center w-[120px]'> ${ (costo * 1.30) * 1.21 } </p>
+            <p className='text-lg font-semibold text-center w-[100px]'> ${Math.round(costo * 1.30)}  </p>
+            <p className='text-lg font-semibold text-center w-[120px]'> ${ Math.round((costo * 1.30) * 1.21) } </p>
             <div className='flex items-center gap-3 w-[120px]'>
-                <button className='bg-blue-600 text-white py-2 px-3 rounded-lg hover:bg-blue-800' onClick={() => router.push(`/edit/${_id}`)}>Edit</button>
-                <button className='bg-red-600 text-white py-2 px-3 rounded-lg hover:bg-red-800' onClick={() => openModal()}>Delete</button>
+                <button className='bg-blue-600 text-white py-2 px-3 rounded-lg hover:bg-blue-800' onClick={() => router.push(`/edit/${_id}`)}><RiEdit2Line /></button>
+                <button className='bg-red-600 text-white py-2 px-3 rounded-lg hover:bg-red-800' onClick={() => openModal()}><RiDeleteBinLine /></button>
             </div>
         </div>
     )
